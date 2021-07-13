9465faf2a5918d65031f5833be65357006bea135
ReDoProgrammer <redoprogrammer@gmail.com>
2021-07-14 03:22:58 +0700

    change password and logout

--- BE/controllers/account_controller.js
+++ BE/controllers/account_controller.js
@@ -3,6 +3,7 @@ const Account = require("../models/account_model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const { authenticateToken } = require("../middlewares/authenticate");
 
 router.get("/init", (req, res) => {
   Account.countDocuments(async (err, count) => {
@@ -85,5 +86,48 @@ router.post("/login", (req, res) => {
       console.log(err);
     });
 });
+
+router.put('/change-password',authenticateToken,(req,res)=>{
+    Account.findOne({username:'admin'},(err,acc)=>{
+      if(err){
+        console.log(new Error(err.message));
+        return;
+      }
+      let {current_password,new_password} = req.body;
+
+      bcrypt.compare(current_password, acc.password, async function (err, usr) {
+        if (err) {
+          return res.status(500).json({
+            msg: "Check failed!",
+            error: new Error(err),
+          });
+        }
+        if (usr) {
+          const hashedPassword = await bcrypt.hash(new_password, 10);
+          Account.findOneAndUpdate({username:'admin'},{
+            password: hashedPassword
+          },{new:true},async (err,acc)=>{
+            if(err){
+              return res.status(500).json({
+                msg:`Can not change password. ${new Error(err.message)}`
+              })
+            }
+            const access_token =await jwt.sign(
+              acc._id.toString(),
+              process.env.ACCESS_TOKEN_SECRET
+            );
+            return res.status(200).json({
+              msg: "Password has changed!",
+              access_token: access_token,
+            });
+          })
+        } else{
+          return res.status(403).json({
+            msg:`Current password not correct!`
+          })
+        }
+      });
+    })
+})
 
 module.exports = router;
